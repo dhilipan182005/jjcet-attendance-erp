085e0cacaf9d91b4f74e42f7c0d833558c8ba5c1
dhilipan182005 <dhilipan182005.com>
2026-08-05 21:13:12 +0530

    Initial commit

--- AttendanceSystem/src/test/java/com/example/attendancesystem/AttendanceSystemApplicationTests.java
+++ AttendanceSystem/src/test/java/com/example/attendancesystem/AttendanceSystemApplicationTests.java
@@ -2,14 +2,27 @@ package com.example.attendancesystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import com.example.attendancesystem.config.AdminBootstrap;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AttendanceSystemApplicationTests {
+
+    @MockitoBean
+    private DataSource dataSource;
+
+    @MockitoBean
+    private AdminBootstrap adminBootstrap;
 
     @Test
     void contextLoads() {
     }
 
 }
+
+
+
